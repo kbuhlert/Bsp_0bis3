16550526ef7cb51245381584e6fc28d0fb755f0e
Kerstin Jungwirth <kbuhlert@gmail.com>
2020-03-07 11:11:29 +0100

    Lösung Bsp0 b) Lichtschalter an aus wenn schalter = 1

--- src/Beispiel_0.java
+++ src/Beispiel_0.java
@@ -1,8 +1,11 @@
 public class Beispiel_0 {
 
     public static void main(String[] args) {
-    String licht = Lichtschalter_a(1);
-        System.out.println(licht);
+    String lichta = Lichtschalter_a(1);      //Ausgabe für Bsp. 0a)
+        System.out.println(lichta);
+
+        String lichtb = Lichtschlter_b(1);
+        System.out.println(lichtb);
     }
 
     public static String Lichtschalter_a (int schalter) {
@@ -11,6 +14,16 @@ public class Beispiel_0 {
             ergebnis = "Licht an!"; }
         else {
             ergebnis = "Licht aus!";}
+        return ergebnis;
+    }
+
+    public static String Lichtschlter_b (int schalter) {
+        String ergebnis;
+        if (schalter == 1) {
+            ergebnis = "Licht an!";
+        } else {
+            ergebnis = "Licht aus!";
+        }
         return ergebnis;
     }
 
