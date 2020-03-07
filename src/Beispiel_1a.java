5be158f46ef41e5c12f82bc302ae1d30a5726075
Kerstin Jungwirth <kbuhlert@gmail.com>
2020-03-07 19:01:45 +0100

    Lösung Bsp1 a) Bestimmung der Gießzeit abhängig von Feuchtigkeit + Code aufgeräumt

--- src/Beispiel_1a.java
+++ src/Beispiel_1a.java
@@ -1,16 +1,19 @@
 public class Beispiel_1a {
     static int giessen (int feuchtigkeit){
-        int giesszeit = 0;
 
-        if (feuchtigkeit<=20){giesszeit=5;}
-        else if (feuchtigkeit<=40 && feuchtigkeit>20){giesszeit=3;}
-        else if (feuchtigkeit<=75 && feuchtigkeit>40){giesszeit=1;}
-        else {giesszeit=0;}
+        int giesszeit;
 
+        if (feuchtigkeit <= 20)
+        {giesszeit = 5;}
+        else if (feuchtigkeit <= 40 && feuchtigkeit > 20)
+        {giesszeit = 3;}
+        else if (feuchtigkeit <= 75 && feuchtigkeit > 40)
+        {giesszeit = 1;}
+        else {giesszeit = 0;}
         return giesszeit;}
 
         public static void main (String [] args){
-        int feuchtigkeit = 10;  //hier den aktuellen Feuchtigkeitsmesswert (%) eingeben
+        int feuchtigkeit = 60;  //hier den aktuellen Feuchtigkeitsmesswert (%) eingeben
         int bsp1a = giessen(feuchtigkeit);
 
             System.out.println("Die Feuchtigkeit wurde mit " + feuchtigkeit + "% bestimmt.");
