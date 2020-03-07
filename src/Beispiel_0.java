a754a34c0259599316c387102956ff21fb96b319
Kerstin Jungwirth <kbuhlert@gmail.com>
2020-03-07 10:53:30 +0100

    Lösung Bsp0 a) Lichtschalter an aus

--- src/Beispiel_0.java
+++ src/Beispiel_0.java
@@ -1,2 +1,17 @@
 public class Beispiel_0 {
+
+    public static void main(String[] args) {
+    String licht = Lichtschalter_a(1);
+        System.out.println(licht);
+    }
+
+    public static String Lichtschalter_a (int schalter) {
+       String ergebnis;
+        if (schalter > 1) {
+            ergebnis = "Licht an!"; }
+        else {
+            ergebnis = "Licht aus!";}
+        return ergebnis;
+    }
+
 }
