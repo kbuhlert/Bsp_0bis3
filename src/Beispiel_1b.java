134417085ecc6fbb79fe85794edbac1b96d19394
Kerstin Jungwirth <kbuhlert@gmail.com>
2020-03-08 22:44:04 +0100

    Lösung Bsp1 b)
    Aufgabe fertig. Zwei Rückgabewerte gehen nicht gelichzeitig. Switch mit Wassermenge, Düngemenge und Giesvolumen.

--- src/Beispiel_1b.java
+++ src/Beispiel_1b.java
@@ -1,12 +1,20 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class Beispiel_1b {
     public static void main (String[] args){
+        int pflanzennr = 2;         //hier eingeben welche Pflanze gegossen werden soll
+        double giessvolumen = 2.5;  //hier das Gesamtvolumen mit dem gegossen werden soll eingeben
+
+        double ergebnis = duengung(pflanzennr,giessvolumen);
+
+        System.out.println("Die Düngemenge beträgt " + ergebnis + "l auf ein Gesamtvolumen von " + giessvolumen + "l");
 
     }
 
-    static double düngung (int pflanzennr, double giessvolumen){     // giessvolumen ist gesamtflüssigkeit,
-        double wassergehalt;
-        double duengemenge;
-        double wassermenge;
+    static double duengung (int pflanzennr, double giessvolumen){     // giessvolumen ist gesamtflüssigkeit,
+        double wassergehalt = 0;
+        double duengemenge = 0;
+        double wassermenge = 0;
 
         switch (pflanzennr){
             case 1:
@@ -29,9 +37,14 @@ public class Beispiel_1b {
                 wassermenge = giessvolumen * wassergehalt;
                 duengemenge = giessvolumen - wassermenge;
                 break;
+            default:
+                wassergehalt = 0.99;
+                wassermenge = giessvolumen * wassergehalt;
+                duengemenge = giessvolumen - wassermenge;
+                break;
         }
         return duengemenge;
-        return wassermenge;
+        //return wassermenge;           //ich kann hier nicht zwei Werte ausgeben, wie geht das?
     }
 
 }
