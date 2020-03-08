6a1a31ac85c0e8e16e495a7b855ea991186ea518
Kerstin Jungwirth <kbuhlert@gmail.com>
2020-03-08 23:23:14 +0100

    Lösung Bsp1 b)
    Aufgabe fertig. Zweite Methode zugefügt. Eine Methode für Düngemenge. Eine Methode für Wassermenge. Switch mit Wassermenge, Düngemenge und Giesvolumen.

--- src/Beispiel_1b.java
+++ src/Beispiel_1b.java
@@ -5,10 +5,42 @@ public class Beispiel_1b {
         int pflanzennr = 2;         //hier eingeben welche Pflanze gegossen werden soll
         double giessvolumen = 2.5;  //hier das Gesamtvolumen mit dem gegossen werden soll eingeben
 
-        double ergebnis = duengung(pflanzennr,giessvolumen);
+        double duengeanteil = duengung(pflanzennr,giessvolumen);
+        double wasseranteil = wasser(pflanzennr,giessvolumen);
 
-        System.out.println("Die Düngemenge beträgt " + ergebnis + "l auf ein Gesamtvolumen von " + giessvolumen + "l");
+        System.out.println("Die Düngemenge beträgt " + duengeanteil + "l und muss mit " + wasseranteil + "l Wasser auf ein Gesamtvolumen von " + giessvolumen + "l aufgefüllt werden.");
 
+
+    }
+
+    static double wasser (int pflanzennr, double giessvolumen){     // giessvolumen ist gesamtflüssigkeit,
+        double wassergehalt = 0;
+        double wassermenge = 0;
+
+        switch (pflanzennr){
+            case 1:
+                wassergehalt = 0.99;
+                wassermenge = giessvolumen * wassergehalt;
+                break;
+            case 2:
+                wassergehalt = 0.95;
+                wassermenge = giessvolumen * wassergehalt;
+                break;
+            case 3:
+                wassergehalt = 1;
+                wassermenge = giessvolumen * wassergehalt;
+                break;
+            case 4:
+                wassergehalt = 0.98;
+                wassermenge = giessvolumen * wassergehalt;
+                break;
+            default:
+                wassergehalt = 0.99;
+                wassermenge = giessvolumen * wassergehalt;
+                break;
+        }
+
+        return wassermenge;           //ich kann hier nicht zwei Werte ausgeben, wie geht das?
     }
 
     static double duengung (int pflanzennr, double giessvolumen){     // giessvolumen ist gesamtflüssigkeit,
@@ -44,7 +76,6 @@ public class Beispiel_1b {
                 break;
         }
         return duengemenge;
-        //return wassermenge;           //ich kann hier nicht zwei Werte ausgeben, wie geht das?
     }
 
 }
